4a97d9693b9700a2184caeffd2fa2f90d4a07b8d
Adam Tait <pair+adam@rally.org>
2012-09-05 15:00:28 -0700

    add operations and decimal in entered numbers

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,31 +7,47 @@
 //
 
 #import "CalculatorViewController.h"
+#import "CalculatorBrain.h"
 
 @interface CalculatorViewController()
 @property (nonatomic) BOOL userIsInMiddleOfEnteringANumber;
+@property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
 @implementation CalculatorViewController
 
-@synthesize display;
-@synthesize userIsInMiddleOfEnteringANumber;
+@synthesize display = _display;
+@synthesize userIsInMiddleOfEnteringANumber = _userIsInMiddleOfEnteringANumber;
+@synthesize brain = _brain;
+
+- (CalculatorBrain *)brain {
+    if(!_brain) _brain = [[CalculatorBrain alloc] init];
+    return _brain;
+}
 
 - (IBAction)digitPressed:(UIButton *)sender {
     NSString *digit = [sender currentTitle];
     
-    if(userIsInMiddleOfEnteringANumber) {
+    if(_userIsInMiddleOfEnteringANumber) {
         self.display.text = [self.display.text stringByAppendingString:digit];
     } else {
         self.display.text = digit;
-        userIsInMiddleOfEnteringANumber = YES;
+        _userIsInMiddleOfEnteringANumber = YES;
     }
 
 }
 - (IBAction)enterPressed {
+    [self.brain pushOperand:[self.display.text doubleValue]];
+    self.userIsInMiddleOfEnteringANumber = NO;
 }
 
 - (IBAction)operationPressed:(UIButton *)sender {
+    if(self.userIsInMiddleOfEnteringANumber) {
+        [self enterPressed];
+    }
+    NSString *operation = sender.currentTitle;
+    double result = [self.brain performOperation:operation];
+    self.display.text = [NSString stringWithFormat:@"%g", result];
 }
 
 //NSLog(@"user touched %@", digit);
